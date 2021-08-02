b63ea7ecf626ac50941d34ce6449916f4d591c6a
scottbw <scott.bradley.wilson@gmail.com>
2021-08-02 14:36:34 +0100

    Modularised code

---
+++ keywords.py
@@ -0,0 +1,27 @@
+from rake_nltk import Rake
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from stopwords import stopwords
+
+nltk.download('wordnet')
+
+# initiate nltk lemmatiser
+lemma = WordNetLemmatizer()
+
+r = Rake(stopwords=stopwords, min_length=1, max_length=4)
+
+
+def rake_implement(x, r):
+    r.extract_keywords_from_text(x)
+    return r.get_ranked_phrases()
+
+
+def add_keywords(df, column):
+    df = df.copy()
+    df['keywords'] = df[column].apply(lambda x: rake_implement(x, r))
+    df['keywords'] = df['keywords'].apply(lambda x: [lemma.lemmatize(y) for y in x])
+    df = df.explode('keywords')
+    df.dropna(subset=['keywords'], inplace=True)
+    return df
+
--- topics.py
+++ topics.py
@@ -8,30 +8,12 @@ from rake_nltk import Rake
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import collections
+from stopwords import stopwords
 
 nltk.download('wordnet')
 
-# initiate stopwords from nltk
-stop_words = stopwords.words('english')
-
-# add additional missing terms
-stop_words.extend(
-    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-     'x', 'y', 'z', "about", "across", "after", "all", "also", "an", "and", "another", "added",
-     "any", "are", "as", "at", "basically", "be", "because", 'become', "been", "before", "being", "between", "both",
-     "but", "by", "came", "can", "come", "could", "did", "do", "does", "each", "else", "every", "either", "especially",
-     "for", "from", "get", "given", "gets",
-     'give', 'gives', "got", "goes", "had", "has", "have", "he", "her", "here", "him", "himself", "his", "how", "if",
-     "in", "into", "is", "it", "its", "just", "lands", "like", "make", "making", "made", "many", "may", "me", "might",
-     "more", "most", "much", "must", "my", "never", "provide",
-     "provides", "perhaps", "no", "now", "of", "on", "only", "or", "other", "our", "out", "over", "re", "said", "same",
-     "see", "should", "since", "so", "some", "still", "such", "seeing", "see", "take", "than", "that", "the", "their",
-     "them", "then", "there",
-     "these", "they", "this", "those", "through", "to", "too", "under", "up", "use", "using", "used", "underway",
-     "very", "want", "was", "way", "we", "well", "were", "what", "when", "where", "which", "while", "whilst", "who",
-     "will", "with", "would", "you", "your",
-     "nothing", "idk"
-     'etc', 'via', 'eg', 'e g', 'ie'])
+# initiate stopwords
+stop_words = stopwords
 
 # initiate nltk lemmatiser
 wordnet_lemmatizer = WordNetLemmatizer()
