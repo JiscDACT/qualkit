ced61471347a67d6c1cc27f9951e74c49607eaa4
scottbw <scott.bradley.wilson@gmail.com>
2021-08-03 07:42:12 +0100

    Added tests

--- test/test_clean.py
+++ test/test_clean.py
@@ -1,7 +1,14 @@
 import pytest
-from qualkit.clean import replace_dont_knows
+from qualkit.clean import replace_dont_knows, replace_domain_terms
 
 
 def test_replace_dont_knows():
     text = replace_dont_knows('i dont know', 'idk')
     assert text == 'idk'
+
+
+def test_replace_domain_terms():
+    text = 'I use Blackboard'
+    domain = ['Blackboard', 'Moodle']
+    text = replace_domain_terms(text, domain, "a VLE")
+    assert text == 'I use a VLE'
