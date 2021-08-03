0f8fff8c739299f4bd48dfb3e5fa82f3ed985492
scottbw <scott.bradley.wilson@gmail.com>
2021-08-03 07:37:40 +0100

    Added tests

--- test/test_clean.py
+++ test/test_clean.py
@@ -3,5 +3,5 @@ from qualkit.clean import replace_dont_knows
 
 
 def test_replace_dont_knows():
-    text = replace_dont_knows('I dont know', 'idk')
+    text = replace_dont_knows('i dont know', 'idk')
     assert text == 'idk'
