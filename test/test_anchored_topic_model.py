442e8c8bc53b212cacfbd412c15cf2738dddcea9
scottbw <scott.bradley.wilson@gmail.com>
2021-08-05 20:22:48 +0100

    Changed path for test data

--- test/test_anchored_topic_model.py
+++ test/test_anchored_topic_model.py
@@ -2,7 +2,7 @@ from qualkit import anchored_topic_model
 
 
 def test_load_topics():
-    df = anchored_topic_model.load_topics('topics.csv')
+    df = anchored_topic_model.load_topics('./topics.csv')
     assert df['topic_name'].size == 3
     assert df['anchors'].loc[0] == ['apple', 'banana']
     assert df['anchors'].loc[2] == ['potato', 'carrot', 'onion']
